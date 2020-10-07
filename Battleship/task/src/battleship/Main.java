37209621b240f22d71ff34dcb585a15327d98c62
Sergey Mikhaylov <bodmode@gmail.com>
2020-10-07 15:44:30 +0300

    create draft of constructor methods for Ship class create getShips method

--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -1,5 +1,8 @@
 package battleship;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         Field field = createField();
@@ -7,6 +10,7 @@ public class Main {
 
     private static Field createField() {
         Field field = new Field();
+        Map<String, Integer> shipTypes = Ship.getShips();
         return field;
     }
 }
--- Battleship/task/src/battleship/Ship.java
+++ Battleship/task/src/battleship/Ship.java
@@ -1,7 +1,35 @@
 package battleship;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Ship {
     private int[] coordinates;
     private int size;
 
+    public Ship(int size, String name) {
+        this.size = size;
+        this.coordinates = inputCoordinates(size,name);
+    }
+
+    public int[] inputCoordinates(int size, String name) {
+        System.out.printf("Enter the coordinates of the %s (%d cells):%n");
+        boolean incorrectInput = true;
+        Scanner scanner = new Scanner(System.in);
+        while (incorrectInput) {
+            String[] coordinates = scanner.nextLine().split(" ");
+        }
+        return new int[0];
+    }
+
+    public static Map<String, Integer> getShips() {
+        Map<String, Integer> shipTypes = new LinkedHashMap<>();
+        shipTypes.put("Aircraft Carrier", 5);
+        shipTypes.put("Battleship", 4);
+        shipTypes.put("Submarine", 3);
+        shipTypes.put("Cruiser", 3);
+        shipTypes.put("Destroyer", 2);
+        return shipTypes;
+    }
 }
