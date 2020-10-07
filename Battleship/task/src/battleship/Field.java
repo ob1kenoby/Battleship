ca4bce19c58d73e77e103efb394396e3741f01ce
Sergey Mikhaylov <bodmode@gmail.com>
2020-10-07 15:21:01 +0300

    implement prepareField method
    The method prepareField of Field class creates a string with the actual
    values of the field.

--- Battleship/task/src/battleship/Field.java
+++ Battleship/task/src/battleship/Field.java
@@ -14,4 +14,19 @@ public class Field {
             }
         }
     }
+
+    private String prepareField() {
+        StringBuilder fieldToOutput = new StringBuilder("  1 2 3 4 5 6 7 8 9 10\n");
+        String[] letters = "ABCDEFGHIJ".split("");
+
+        for (int i = 0; i < 10; i++) {
+            fieldToOutput.append(letters[i] + " ");
+            for (int j = 0; j < 10; j++) {
+                fieldToOutput.append(this.field[i][j] + " ");
+            }
+            fieldToOutput.append("\n");
+        }
+
+        return fieldToOutput.toString();
+    }
 }
