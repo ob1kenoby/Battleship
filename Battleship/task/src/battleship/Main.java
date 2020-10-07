12f7aaa383ef02815df2c8788eea5195e39f1cfe
Sergey Mikhaylov <bodmode@gmail.com>
2020-10-07 15:26:21 +0300

    create draft of createField method

--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -2,6 +2,11 @@ package battleship;
 
 public class Main {
     public static void main(String[] args) {
+        Field field = createField();
+    }
+
+    private static Field createField() {
         Field field = new Field();
+        return field;
     }
 }
