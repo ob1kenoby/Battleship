9cabd56ae0eb8387aac0b89e2563be7d167cef22
Sergey Mikhaylov <bodmode@gmail.com>
2020-10-13 06:06:00 +0300

    make field property of Player class final

--- Battleship/task/src/battleship/Player.java
+++ Battleship/task/src/battleship/Player.java
@@ -4,10 +4,10 @@ import java.util.Map;
 
 class Player {
 
-    private Field field;
+    private final Field FIELD;
 
     public Player() {
-        this.field = new Field();
+        this.FIELD = new Field();
         initializeField();
     }
 
@@ -15,15 +15,15 @@ class Player {
         Map<String, Integer> shipSizes = Ship.getShips();
         for (String shipType : shipSizes.keySet()) {
             System.out.printf("Enter the coordinates of the %s (%d cells):%n%n", shipType, shipSizes.get(shipType));
-            boolean incorrectPlacement = true;
+            boolean incorrectPlacement;
             do {
                 Ship ship = new Ship(shipType);
-                incorrectPlacement = field.addShipToField(ship);
+                incorrectPlacement = FIELD.addShipToField(ship);
             } while (incorrectPlacement); {
                 System.out.println("Error! You placed it too close to another one. Try again:\n");
             }
         }
-        field.printField(false);
+        FIELD.printField(false);
     }
 
 }
